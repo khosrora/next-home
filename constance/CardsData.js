2dc29fbb06331e2961b0065317198745e0f2b9d0
khosrora <khosrora153333@gmail.com>
2022-12-02 15:33:21 +0330

    first commit

---
+++ constance/CardsData.js
@@ -0,0 +1,26 @@
+
+
+
+
+export const data = [
+    {
+        title: "Documentation ",
+        text: "Find in-depth information about Next.js features and API.",
+        link: "https://nextjs.org/docs"
+    },
+    {
+        title: "Learn ",
+        text: "Learn about Next.js in an interactive course with quizzes!",
+        link: "https://nextjs.org/learn"
+    },
+    {
+        title: "Examples ",
+        text: "Discover and deploy boilerplate example Next.js projects.",
+        link: "https://github.com/vercel/next.js/tree/canary/examples"
+    },
+    {
+        title: "Deploy ",
+        text: "Instantly deploy your Next.js site to a public URL with Vercel.",
+        link: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+    },
+]
